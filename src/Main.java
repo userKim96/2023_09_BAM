55326b7d9559ecff045e32b93cca786a1bd9a875
userKim96 <rlaclgnsska002@gmail.com>
2023-09-12 15:27:33 +0900

    게시물 유뮤 판결

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,51 +8,63 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
-
+		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<Article>();
+		
 		while (true) {
-			System.out.printf("명령어 )");
+			
+			System.out.printf("명령어 ) ");
 			String command = sc.nextLine();
-			
-			if(command.length() == 0) {
-				System.out.println("명령어를 입력하세요");
-			} else	if(command.equals("exit")) {
+
+			if (command.length() == 0) {
+				System.out.println("너 명령어 입력 안했어");
+				continue;
+			}
+
+			if (command.equals("exit")) {
 				break;
-			} else if(command.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
+			}
+
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+				}
+				else {
+					System.out.println("게시글 있던데???");
+				}
+
 			} else if (command.equals("article write")) {
-				System.out.println("제목 : ");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
 				String title = sc.nextLine();
-				
-				System.out.println("내용 : ");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+				lastArticleId++;
 			} else {
-				System.out.println("존재하지 않는 명령어입니다. ");
-			}
-			
-			
-			
-			if(command.equals("exit")) {
-				break;
-			}
-			
-			if(command.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
-			} else if (command.equals("article write")) {
-				int n = 0;
-				n++;
-				System.out.println("제목 : ");
-				String title = sc.nextLine();
-				
-				System.out.println("내용 : ");
-				String body = sc.nextLine();
-				
-				System.out.printf("%d번글이 생성되었습니다.", n);
+				System.out.println("존재하지 않는 명령어입니다");
+				continue;
 			}
 		}
 
-		System.out.println("== 프로그램 끝 ==");
-		
+		System.out.println("== 프로그램 종료 ==");
+
 		sc.close();
 	}
 }
 
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
