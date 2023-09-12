24336ef4bc71966117e04da8bd11008deca08006
userKim96 <rlaclgnsska002@gmail.com>
2023-09-12 13:44:19 +0900

    명령어 입력 연습

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.printf("명령어 )");
+			String command = sc.nextLine();
+			
+			if(command.length() == 0) {
+				System.out.println("명령어를 입력하세요");
+			} else {
+				System.out.println("존재하지 않는 명령어입니다. ");
+			}
+			
+			
+			
+			if(command.equals("exit")) {
+				break;
+			}
+			
+			if(command.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			} else if (command.equals("article write")) {
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+			}
+		}
+
+		System.out.println("== 프로그램 끝 ==");
 		
-//		String cmd = sc.nextLine();
-//		int cmd = sc.nextInt();
-		String cmd = sc.next();
-		
-		System.out.println("[" + cmd + "]");
-		
-		System.out.println("== 프로그램 시작 ==");
-sysout
+		sc.close();
 	}
 }
+
